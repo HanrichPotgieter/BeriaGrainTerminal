72c6068f3f387a04b7d43e214dc0423be7242528
Hanrich <Hanrichp@gmail.com>
2016-06-24 14:15:01 +0200

    added Function Block 14

--- elements/FB12.js
+++ elements/FB12.js
@@ -16,12 +16,34 @@ module.exports = {
         //===================================================>  
         case 7056:
             switch(status) {
-                case 513:
+                case 1:
+                // StStopped
                     response.color = "gray";
                     callback(response);
                     break;
+                case 2:
+                // StStarting
+                    response.color = "lightgreen";
+                    callback(response);
+                    break;
                 case 3:
+                // StStartedFwd
                     response.color = "green";
+                    callback(response);
+                    break;
+                case 5:
+                // StStartedRev
+                    response.color = "green";
+                    callback(response);
+                    break;
+                case 7:
+                // StStopping
+                    response.color = "cyan";
+                    callback(response);
+                    break;
+                case 32:
+                // StFault
+                    response.color = "red";
                     callback(response);
                     break;
                 default:callback(response);
@@ -30,7 +52,5 @@ module.exports = {
         //<===================================================
         default:callback(response);
     }
-
-
   }
 };
