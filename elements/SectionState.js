c59ac7f56c16ca084b3e0acd9db3779b4cbe0067
Hanrich <Hanrichp@gmail.com>
2016-06-27 14:32:06 +0200

    added two new state files

---
+++ elements/SectionState.js
@@ -0,0 +1,37 @@
+module.exports = {
+  getStatus: function (status,callback) {
+    var response = {
+    color:'yellow',
+    status:'unknown'
+    }
+    switch(status){
+        case 1:
+            response.color="white"
+            response.status="Passive"
+        break;
+        case 2:
+            response.color="lightgreen"
+            response.status="Waiting"
+        break;
+        case 4:
+            response.color="lightgreen"
+            response.status="Active"
+        break;
+        case 16:
+            response.color="cyan"
+            response.status="Emptying"
+        break;
+        case 32:
+            response.color="yellow"
+            response.status="Emptied"
+        break;
+        case 64:
+            response.color="lightgreen"
+            response.status="Idling"
+        break;
+        default:callback(response);
+
+    }
+    callback(response);
+  }
+};
