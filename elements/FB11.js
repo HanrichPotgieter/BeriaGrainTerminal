143f241a4d0ab168c6ad8c37c0784adf900a99f5
Hanrich <Hanrichp@gmail.com>
2016-06-28 11:40:09 +0200

    Sync with others

---
+++ elements/FB11.js
@@ -0,0 +1,56 @@
+//===================================================>
+// Motor Function Block
+//===================================================>  
+module.exports = {
+  getStatus: function (data,status,callback) {
+
+    var response = {
+    color:'yellow',
+    status:'Unknown Status'
+    }
+
+    switch(parseInt(data.ptype))
+    { 
+        //===================================================>
+        // ptype 7002
+        //===================================================>  
+        case 7002:
+            switch(status) {
+                case 1:
+                // StStopped
+                    response.color = "gray";
+                    response.status = "Stopped";
+                    callback(response);
+                    break;
+                case 2:
+                // StStarting
+                    response.color = "lightgreen";
+                    response.status = "Starting";
+                    callback(response);
+                    break;
+                case 3:
+                // StStartedFwd
+                    response.color = "green";
+                    response.status = "Started";
+                    callback(response);
+                    break;
+                case 5:
+                // StStartedRev
+                    response.color = "grey";
+                    response.status = "False";
+                    callback(response);
+                    break;
+                case 32:
+                // StStopping
+                    response.color = "red";
+                    response.status = "Fault";
+                    callback(response);
+                    break;
+                default:callback(response);
+            }
+        break;
+        //<===================================================
+        default:callback(response);
+    }
+  }
+};
---
+++ elements/FB16.js
@@ -0,0 +1,52 @@
+//===================================================>
+// Motor Function Block
+//===================================================>  
+module.exports = {
+  getStatus: function (data,status,callback) {
+
+    var response = {
+    color:'yellow',
+    status:'Unknown Status'
+    }
+    //console.log(status);
+    switch(parseInt(data.ptype))
+    { 
+        //===================================================>
+        // ptype 7231
+        //===================================================>  
+        case 7231:
+            switch(status-512) {
+                case 1:
+                    response.color = "gray";
+                    response.status = "Minimum";
+                    callback(response);
+                    break;
+                case 3:
+                    response.color = "lightgreen";
+                    response.status = "Normal Pressure";
+                    callback(response);
+                    break;
+                case 4:
+                    response.color = "green";
+                    response.status = "High Pressure";
+                    callback(response);
+                    break;
+                case 5:
+                    response.color = "red";
+                    response.status = "Over Pressure";
+                    callback(response);
+                    break;
+                case 32:
+                // StFault
+                    response.color = "red";
+                    response.status = "Fault";
+                    callback(response);
+                    break;
+                default:callback(response);
+            }
+        break;
+        //<===================================================
+        default:callback(response);
+    }
+  }
+};
