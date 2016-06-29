8b22256cc7cb8e48724b985c3eb23622ccfdbb92
Hanrich <Hanrichp@gmail.com>
2016-06-29 14:26:48 +0200

    Updated FB14

--- elements/FB14.js
+++ elements/FB14.js
@@ -8,81 +8,39 @@ module.exports = {
     color:'yellow',
     status:'Unknown Status '+status
     }
-    switch(parseInt(data.ptype))
-    { 
-        //===================================================>
-        // ptype 7177
-        //===================================================>  
-        case 7177:    
-            switch(status) {
-                case 1:
-                // StFalse
-                    response.color = "gray";
-                    callback(response);
-                    break;
-                case 2:
-                // StCtoTrue
-                    response.color = "lightgreen";
-                    callback(response);
-                    break;
-                case 3:
-                // StTrue
-                    response.color = "green";
-                    callback(response);
-                    break;
-                case 4:
-                // StCtoFalse
-                    response.color = "lightgreen";
-                    callback(response);
-                    break;
-                case 515:
-                // StTrue
-                    response.color = "green";
-                    callback(response);
-                    break;
-                case 32:
-                // StFault
-                    response.color = "red";
-                    callback(response);
-                    break;
-                default:callback(response);
-            }
-        break;
-        //<===================================================
-        //===================================================>
-        // ptype 7165
-        //===================================================>  
-        case 7165:   
-            switch(status) {
-                case 256:
-                    response.color = "gray";
-                    response.status = "Uncovered";
-                    callback(response);
-                    break;
-                case 512:
-                    response.color = "lightgreen";
-                    response.status = "Change To Covered";
-                    callback(response);
-                    break;
-                case 1024:
-                    response.color = "green";
-                    response.status = "Covered";
-                    callback(response);
-                    break;
-                case 2048:
-                    response.color = "lightgreen";
-                    response.status = "Change To Uncovered";
-                    callback(response);
-                    break;
-                case 4096:
-                    response.color = "red";
-                    response.status = "Fault";
-                    callback(response);
-                    break;
-                default:callback(response);
-            }
-        break;
-        //<===================================================
+    //===================================================>
+    // ptype 7177
+    //===================================================>    
+    switch(status) {
+        case 1:
+        // StFalse
+            response.color = "gray";
+            response.status = "False";
+            callback(response);
+            break;
+        case 2:
+        // StCtoTrue
+            response.color = "lightgreen";
+            response.status = "Change To True";
+            callback(response);
+            break;
+        case 3:
+        // StTrue
+            response.color = "green";
+            response.status = "True";
+            callback(response);
+            break;
+        case 4:
+        // StCtoFalse
+            response.color = "lightgreen";
+            response.status = "Change To False";
+            callback(response);
+            break;
+        case 32:
+        // StFault
+            response.color = "red";
+            callback(response);
+            break;
         default:callback(response);
     }
   }
