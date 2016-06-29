722d9918838658ab8252089f1c78bd0ac3c6d6c3
Hanrich <Hanrichp@gmail.com>
2016-06-29 14:53:16 +0200

    testing

--- elements/FB14.js
+++ elements/FB14.js
@@ -8,6 +8,10 @@ module.exports = {
     color:'yellow',
     status:'Unknown Status '+status
     }
+    if(status % 512 > 0)
+    {
+        status = status - 512;
+    }
     switch(status) {
         case 1:
         // StFalse
