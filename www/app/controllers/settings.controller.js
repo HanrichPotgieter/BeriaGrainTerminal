5004bd7b9de07036b57e04b7128409067fb442bf
Hanrich <Hanrichp@gmail.com>
2016-06-29 09:08:52 +0200

    working on line settings

--- www/app/controllers/settings.controller.js
+++ www/app/controllers/settings.controller.js
@@ -1,7 +1,38 @@
 angular
     .module('app')
     .controller('SettingsCtrl', function ($scope) {
-        $scope.plc = {
-            ip:'10.0.0.2'
+        $scope.lines = [{
+                name:"Intake",
+                DB:600,
+                sections:[{
+                    name:"Wheat intake",
+                    DB:601
+                }]
+            },
+            {
+                name:"Intake",
+                DB:600,
+                sections:[{
+                    name:"Wheat intake",
+                    DB:601
+                }]
+            }]
+        $scope.toggleLine = function(line){
+            if(line.show)
+                line.show = false;
+            else
+                line.show = true;
+        }
+        $scope.addLine = function(line){
+
+        }
+        $scope.removeLine = function(line){
+            
+        }
+        $scope.addSection = function(line,section){
+            
+        }
+        $scope.removeSection = function(line,section){
+            
         }
     });
