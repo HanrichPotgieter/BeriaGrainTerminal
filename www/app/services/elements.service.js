ffcb8a2697a6eb9fca4b49465d649836d02be029
Hanrich <Hanrichp@gmail.com>
2016-07-08 18:08:35 +0200

    trying something diffrent

--- www/app/services/elements.service.js
+++ www/app/services/elements.service.js
@@ -24,14 +24,14 @@ angular
 
     var updateElement = function(element,index,data){
         //debugger;
-        d3.select(parent).style('fill',data[index].status.color);
-        //d3.select(element.parentNode).selectAll('*').style('fill',data[index].status.color).style('transition','fill 2.0s ease');
+        //d3.select(parent).style('fill',data[index].status.color);
+        d3.select(element.parentNode).selectAll('*').style('fill',data[index].status.color).style('transition','fill 2.0s ease');
     }
 
     socket.on('updateElements',function(data) {
         console.log('Updating elements');
         var sel = d3.select(document.getElementById("image").contentDocument).selectAll("*");
-        sel.selectAll("*").select("desc").each(function () {
+        sel.select("desc").each(function () {
             if(isJsonString(this.textContent)){
                 var element= JSON.parse(this.textContent);
                 for(x in data){
