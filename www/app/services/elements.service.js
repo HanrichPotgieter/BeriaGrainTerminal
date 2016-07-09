8767c60879bf488e8e8db2cd9112db94ebbff834
Hanrich <Hanrichp@gmail.com>
2016-07-09 12:05:17 +0200

    Checking everything

--- www/app/services/elements.service.js
+++ www/app/services/elements.service.js
@@ -149,7 +149,11 @@ angular
             if(isJsonString(this.textContent)){
                 var element= JSON.parse(this.textContent);
                 for(x in data){
-                    if(data[x].name == element.name && data[x].type == element.type){
+                    if(
+                     data[x].name == element.name
+                     && data[x].type == element.type
+                     && data[x].description == element.description
+                     ){
                         (function(x,element,data,item){
                             updateElement(item,x,data,element);
                         })(x,this,data,element); 
