4353bcc77b61df106b542f566cc5e79e84c185a6
Hanrich <Hanrichp@gmail.com>
2016-07-11 15:33:10 +0200

    Scale data is now being logged

--- elements/scaleData.js
+++ elements/scaleData.js
@@ -2,6 +2,8 @@
 module.exports = {
     init:function(db,Datastore,s7client,directory){
         var scales = [];
+        var limit = 3600;
+
         var readFlowrate = function(DB,OFFSET,database){
             if(s7client.Connected()){
                 s7client.DBRead(parseInt(DB),parseInt(OFFSET)+26,4,function(err,data){
@@ -13,18 +15,40 @@ module.exports = {
                     database.insert({
                         dateTime:new Date(),
                         value:flowrate
-                    })
+                    });
                 });    
             }
+            
+            database.count({}, function (err, count) {
+                //console.log(count);
+                if(count >= limit){
+                    database.find({}).sort({dateTime:-1}).limit(2).exec(function(err,doc){
+                        for(y in doc){
+                             database.remove(doc[y]);
+                        }
+                    });
+                }
+            });
+
         }
+
+        var readData = function(){
+            for(x in scales){
+                var scale = scales[x];
+                readFlowrate(scale.DB,scale.OFFSET,scale.dbFlow);
+            }
+            setTimeout(readData,5000);
+        }
+
         var initilizeScales = function(){
             for(x in scales){
                 var scale = scales[x];
                 scale.dbFlow = new Datastore({ filename: directory + '/database/scales/'+scale.name+'_Flowrate_data', autoload: true });
-                readFlowrate(scale.DB,scale.OFFSET,scale.dbFlow);
             }
+            readData();
         };
-        console.log('Starting scale data');
+
+        console.log('Starting scale data logging');
         db.find({type:'FB40'},function(err,docs){
             if(err) console.log(err);
             scales = docs;
